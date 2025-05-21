2b8efeb37a4a0714ce5775685c6baac62cc0e4dc
DavidLaurea <146191168+DavidLaurea@users.noreply.github.com>
2025-05-21 19:31:44 +0300

    Update auth.ts

--- nextjs-dashboard/auth.ts
+++ nextjs-dashboard/auth.ts
@@ -5,8 +5,6 @@ import { z } from 'zod';
 import type { User } from '@/app/lib/definitions';
 import bcrypt from 'bcrypt';
 import postgres from 'postgres';
-import NeonAdapter from "@auth/neon-adapter"
-import { Pool } from "@neondatabase/serverless"
  
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
  
@@ -22,28 +20,25 @@ async function getUser(email: string): Promise<User | undefined> {
  
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
-  const pool = new Pool({ connectionString: process.env.DATABASE_URL })
-  return {
-    adapter: NeonAdapter(pool),
-    providers: [
-      Credentials({
-        async authorize(credentials) {
-          const parsedCredentials = z
-            .object({ email: z.string().email(), password: z.string().min(6) })
-            .safeParse(credentials);
-  
-          if (parsedCredentials.success) {
-            const { email, password } = parsedCredentials.data;
-            const user = await getUser(email);
-            if (!user) return null;
-            const passwordsMatch = await bcrypt.compare(password, user.password);
-  
-            if (passwordsMatch) return user;
-          }
-  
-          console.log('Invalid credentials');
-          return null;
-        },
-      }),
-    ],
-}});
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials);
+ 
+        if (parsedCredentials.success) {
+          const { email, password } = parsedCredentials.data;
+          const user = await getUser(email);
+          if (!user) return null;
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+ 
+          if (passwordsMatch) return user;
+        }
+ 
+        console.log('Invalid credentials');
+        return null;
+      },
+    }),
+  ],
+});
